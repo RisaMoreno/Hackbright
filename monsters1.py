f34035fa06bb46d2d4db158517dbba14f047087b
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-08 20:36:24 -0700

    Update monsters1.py

--- monsters1.py
+++ monsters1.py
@@ -1,4 +1,4 @@
-class Monsters(object):
+class Monster(object):
 	def __init__(self, name, attack_1, attack_2, attack_3):
 		self.name = name
 		self.attackname_1 = attackname_1
@@ -8,3 +8,8 @@ class Monsters(object):
 		self.attackpower_2 = attackpower_2
 		self.attackpower_3 = attackpower_3
 		
+monster_hellhound = Monster("Hellhound", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
+monster_wendigo = Monster("Wendigo", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
+monster_hexenwulf = Monster("Hexenwulf", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
+monster_dragon = Monster("Dragon", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
+monster_gryphon = Monster("Gryphon", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
