f0ed298fc7ad3f8a817229c6ffef48bc993b0f22
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-08 20:01:36 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,6 +1,9 @@
 class Players(object):
 	def __init__(self, title, attack1, attack2, attack3):
 		self.title = title
-		self.attack1 = attack1
-		self.attack2 = attack2
-		self.attack3 = attack3
+		self.attackname1 = attackname1
+		self.attackname2 = attackname2
+		self.attackname3 = attackname3
+		self.attackpower1 = attackpower1
+		self.attackpower2 = attackpower2
+		self.attackpower3 = attackpower3
