34d4e98117a507087d031f386abd6d3ab1f33dea
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-08 20:00:25 -0700

    Update monsters1.py

--- monsters1.py
+++ monsters1.py
@@ -1,6 +1,10 @@
 class Monsters(object):
 	def __init__(self, name, attack_1, attack_2, attack_3):
 		self.name = name
-		self.attack_1 = attack_1
-		self.attack_2 = attack_2
-		self.attack_3 = attack_3
+		self.attackname_1 = attackname_1
+		self.attackname_2 = attackname_2
+		self.attackname_3 = attackname_3
+		self.attackpower_1 = attackpower_1
+		self.attackpower_2 = attackpower_2
+		self.attackpower_3 = attackpower_3
+		
