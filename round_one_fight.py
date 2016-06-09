346978ff116b8b929d9d031f00d6a65b0424dba4
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-08 20:21:36 -0700

    Update round_one_fight.py

--- round_one_fight.py
+++ round_one_fight.py
@@ -1,2 +1,24 @@
 import players
 import monsters1
+
+playerhp = 10
+monsterhp = 10
+
+playerdeath = False
+monsterdeath = False
+
+while playerhp > 0:
+  if playerhp <= 0:
+    playerdeath = True
+  else:
+    #keep playing
+if(playerdeath):
+  print "You have died! Play again y/n?"
+
+while monsterhp > 0:
+  if monsterhp <= 0:
+    monsterdeath = True
+  else:
+    #keep playing
+if(monsterdeath):
+  print "You have slain the beast! All hail! Play again y/n?"
