1be8dc2d4be6779d5e4cdbefea3f8285db2d8367
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-13 14:42:11 -0700

    Update round_one_fight.py

--- round_one_fight.py
+++ round_one_fight.py
@@ -7,8 +7,19 @@ monsterhp = 20
 playerdeath = False
 monsterdeath = False
 
-if raw_input="n":
-  quit()
+pc = raw_input("What character do you want to play? \n Cleric \n Fighter \n Ranger \n Rogue \n Wizard")
+  
+
+  
+def monster_damage(monsterhp):
+  global monsterhp
+  monsterhp = monsterhp - #playerattack
+  return monsterhp
+
+def player_damage(playerhp):
+  global playerhp
+  playerhp = playerhp - #monsterattack
+  return playerhp
 
 while playerhp > 0:
   if playerhp <= 0:
@@ -16,7 +27,11 @@ while playerhp > 0:
   else:
     #keep playing
 if(playerdeath):
-  print "You have died! Play again y/n?"
+  end = raw_input("You have died! Play again y/n? ")
+  if end="n":
+  quit()
+  else:
+    #play again
 
 while monsterhp > 0:
   if monsterhp <= 0:
@@ -24,4 +39,8 @@ while monsterhp > 0:
   else:
     #keep playing
 if(monsterdeath):
-  print "You have slain the beast! All hail! Play again y/n?"
+  end = raw_input("You have slain the beast! All hail! Play again y/n?" )
+  if end="n":
+  quit()
+  else:
+    #play again
