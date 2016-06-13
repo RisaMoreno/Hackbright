b18e995d63159a51dfe79146f9d261cf0a9fc27a
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-13 14:47:15 -0700

    Update round_one_fight.py

--- round_one_fight.py
+++ round_one_fight.py
@@ -15,32 +15,38 @@ def monster_damage(monsterhp):
   global monsterhp
   monsterhp = monsterhp - #playerattack
   return monsterhp
+  while monsterhp > 0:
+    if monsterhp <= 0:
+      monsterdeath = True
+    else:
+      #keep playing
+  if(monsterdeath):
+    end = raw_input("You have slain the beast! All hail! Play again y/n?" )
+    if end="n":
+      quit()
+    else:
+      #play again
 
 def player_damage(playerhp):
   global playerhp
   playerhp = playerhp - #monsterattack
   return playerhp
+  while playerhp > 0:
+    if playerhp <= 0:
+      playerdeath = True
+    else:
+      #keep playing
+  if(playerdeath):
+    end = raw_input("You have died! Play again y/n? ")
+    if end="n":
+      quit()
+    else:
+      #play again
 
-while playerhp > 0:
-  if playerhp <= 0:
-    playerdeath = True
-  else:
-    #keep playing
-if(playerdeath):
-  end = raw_input("You have died! Play again y/n? ")
-  if end="n":
-  quit()
-  else:
-    #play again
 
-while monsterhp > 0:
-  if monsterhp <= 0:
-    monsterdeath = True
-  else:
-    #keep playing
-if(monsterdeath):
-  end = raw_input("You have slain the beast! All hail! Play again y/n?" )
-  if end="n":
-  quit()
-  else:
-    #play again
+    
+def __main__:
+  
+    
+if __name__ = "__main__":
+  main()
