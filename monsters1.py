deeb202bb772754b33166c74092abed15d3c7109
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-13 20:08:26 -0700

    Update monsters1.py

--- monsters1.py
+++ monsters1.py
@@ -1,15 +1,14 @@
 class Monster(object):
-	def __init__(self, name, attack_1, attack_2, attack_3):
+	def __init__(self, name):
 		self.name = name
-		self.attackname_1 = attackname_1
-		self.attackname_2 = attackname_2
-		self.attackname_3 = attackname_3
-		self.attackpower_1 = attackpower_1
-		self.attackpower_2 = attackpower_2
-		self.attackpower_3 = attackpower_3
+		self.monsterattack={"claws you":1, "bites you":3, "takes a bite out of you":5}
 		
-monster_hellhound = Monster("Hellhound", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
-monster_wendigo = Monster("Wendigo", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
-monster_hexenwulf = Monster("Hexenwulf", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
-monster_dragon = Monster("Dragon", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
-monster_gryphon = Monster("Gryphon", "claws you", "bites you", "takes a bite out of you", 1, 3, 5)
+monster_hellhound = Monster("Hellhound")
+monster_wendigo = Monster("Wendigo")
+monster_hexenwulf = Monster("Hexenwulf")
+monster_dragon = Monster("Dragon")
+monster_gryphon = Monster("Gryphon")
+
+from random import choice
+
+choice(Monster)
