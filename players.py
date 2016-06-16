9599426cbbdbf4886bb17ad4b6363244a6e4434f
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-15 21:02:35 -0700

    Update players.py

--- players.py
+++ players.py
@@ -2,15 +2,15 @@ class Player(object):
 	def __init__(self, title):
 		self.title = title
 		if self.title == Wizard:
-			attacks = [{"casts a magic missile":1}, {"casts ray of frost":3}, {"casts lightning":5}]
+			attack_wizard = [{"casts a magic missile":1}, {"casts ray of frost":3}, {"casts lightning":5}]
 		if self.title == Fighter:
-			attacks = [{"slashes at":1}, {"stabs":3}, {"hacks a piece off of":5,}]
+			attack_fighter = [{"slashes at":1}, {"stabs":3}, {"hacks a piece off of":5,}]
 		if self.title == Rogue:
-			attacks = [{"poison darts":1}, {"backstabs":3}, {"garrotes":5,}]
+			attack_rogue = [{"poison darts":1}, {"backstabs":3}, {"garrotes":5,}]
 		if self.title == Cleric:
-			attacks = [{"uses shadow aura":1}, {"casts psyche bolt":3}, {"uses soul drain":5}]
+			attack_cleric = [{"uses shadow aura":1}, {"casts psyche bolt":3}, {"uses soul drain":5}]
 		if self.title == Ranger:
-			attacks = [{"fires an arrow":1}, {"stabs":3}, {"a fire arrow":5}]
+			attack_ranger = [{"fires an arrow":1}, {"stabs":3}, {"a fire arrow":5}]
 
 player_wizard = Player("Wizard")
 player_fighter = Player("Fighter")
@@ -23,20 +23,21 @@ playerhp = 20
 playerdeath = False
 
 def player_damage(playerhp):
-  global playerhp
-  playerhp = playerhp - monsterattack
-  return playerhp
-  while playerhp > 0:
-    if playerhp <= 0:
-      playerdeath = True
-    else:
-      #loop through monster turn
-  if(playerdeath):
-    end = raw_input("You have died! Play again y/n? ")
-    if end="n":
-      quit()
-    else:
-      #play again
+	global playerhp
+	playerhp = playerhp - monsterattack
+	return playerhp
+	while playerhp > 0:
+		print fightingmonster """attack""" pc "for " """attackvalue""" damage. Ouch!"
+  		if playerhp <= 0:
+  			playerdeath = True
+  		else:
+  			#loop through monster turn
+	if(playerdeath):
+		end = raw_input("You have died! Play again y/n? ")
+		if end="n":
+			quit()
+		else:
+			#play again
       
  player_wizard = Player("Wizard")
  
