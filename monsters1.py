09c149bc721507995ac11ee5efa6659b698a5008
RisaMoreno <Risa.Moreno@gmail.com>
2016-06-20 11:14:10 -0700

    Update monsters1.py

--- monsters1.py
+++ monsters1.py
@@ -2,12 +2,6 @@ class Monster(object):
 	def __init__(self, name):
 		self.name = name
 		self.monsterattack={"claws you":1, "bites you":3, "takes a bite out of you":5}
-		
-monster_hellhound = Monster("Hellhound")
-monster_wendigo = Monster("Wendigo")
-monster_hexenwulf = Monster("Hexenwulf")
-monster_dragon = Monster("Dragon")
-monster_gryphon = Monster("Gryphon")
 
 monsterhp = 20
 
@@ -18,20 +12,26 @@ from random import choice
 fightingmonster = choice(Monster)
 
 def monster_damage(monsterhp):
-	global monsterhp
+#	global monsterhp
 	monsterhp = monsterhp - playerattack
-	return monsterhp
+	
 	while monsterhp > 0:
   		#print pc """attack""" fightingmonster "for " """attackvalue""" damage. Ouch!"
-  	if monsterhp <= 0:
-    		monsterdeath = True
-  	else:
-  		turn_count += 1
-  		print "Player's turn!"
-		#loop through player turn
+  		if monsterhp <= 0:
+    			monsterdeath = True
+  		else:
+  			turn_count += 1
+  			print "Player's turn!"
+			#loop through player turn
 	if(monsterdeath):
 		end = raw_input("You have slain the beast! All hail! Play again y/n?" )
-			if end="n":
-				quit()
-			else:
-				#play again
+		if end == "n":
+			quit()
+		else:
+			print "Play again!"
+		
+monster_hellhound = Monster("Hellhound")
+monster_wendigo = Monster("Wendigo")
+monster_hexenwulf = Monster("Hexenwulf")
+monster_dragon = Monster("Dragon")
+monster_gryphon = Monster("Gryphon")
